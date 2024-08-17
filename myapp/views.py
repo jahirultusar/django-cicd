2d1f9f858cd0637cc3f2ed99f42e3ccc8ba61fa9
Jahirul Tusar <tusar86@gmail.com>
2024-08-17 07:55:47 +0100

    index added

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world!")
-
+    return render(request, 'myapp/index.html')
 
 # Create your views here.
