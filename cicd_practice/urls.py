d9a364c0f8d569c13d8235ae989048a1a43149ea
Jahirul Tusar <tusar86@gmail.com>
2024-08-20 05:59:14 +0100

    version test

--- cicd_practice/urls.py
+++ cicd_practice/urls.py
@@ -22,4 +22,5 @@ from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('version/', views.version_view, name='version'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import os
 
-# from django.http import HttpResponse 
 
 def index(request):
     return render(request, 'myapp/index.html')
 
 
+
+def version_view(request):
+    version_file_path = os.path.join(os.path.dirname(__file__), '../version.txt')
+    try:
+        with open(version_file_path, 'r') as f:
+            version_info = f.read()
+    except FileNotFoundError:
+        version_info = "Version information not available."
+    return HttpResponse(version_info, content_type="text/plain")
