7e02bf7801952bcfda3988fd9cda6e0775619b39
Jahirul Tusar <tusar86@gmail.com>
2024-08-22 20:55:42 +0100

    git hash CD update, versioning fix next

--- myapp/views.py
+++ myapp/views.py
@@ -6,20 +6,15 @@ import subprocess
 
 # def index(request):
 #     return render(request, 'myapp/index.html')
-def index(request):
-    try:
-        git_version, git_hash, git_message, git_date = get_git_info()
-    except Exception as e:
-        git_version, git_hash, git_message, git_date = ("Error fetching version", "unknown", "Error fetching message", "unknown")
 
+def index(request):
+    git_hash, git_message, git_date = get_git_info()
     context = {
-        'git_version': git_version,
-        'git_hash': git_hash,
+        'git_version': git_hash,
         'git_message': git_message,
-        'git_date': git_date
+        'git_date': git_date 
     }
     return render(request, 'myapp/index.html', context)
-
 
 # def get_git_info():
 #     try:
@@ -31,32 +26,12 @@ def index(request):
     
 
 
-# def get_git_info():
-#     try:
-#         commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()[:6]
-#         # Fetching commit message and date with corrected command format
-#         log_output = subprocess.check_output(['git', 'log', '-1', '--pretty=%B%n%ad', '--date=local']).decode('ascii').strip()
-#         commit_message, commit_date = log_output.split('\n', 1)  # Splits the output into message and date
-#         return commit_hash, commit_message, commit_date
-#     except subprocess.CalledProcessError:
-#         return "unknown", "No commit message available", "Date unknown"
-
-
-
-#current
-
 def get_git_info():
     try:
-        # Fetch the latest Git tag
-        latest_tag = subprocess.check_output(['git', 'describe', '--tags', '--abbrev=0']).decode('ascii').strip()
-
-        # Fetch the commit hash
         commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()[:6]
-
-        # Fetch the commit message and date
+        # Fetching commit message and date with corrected command format
         log_output = subprocess.check_output(['git', 'log', '-1', '--pretty=%B%n%ad', '--date=local']).decode('ascii').strip()
         commit_message, commit_date = log_output.split('\n', 1)  # Splits the output into message and date
-        
-        return latest_tag, commit_hash, commit_message, commit_date
+        return commit_hash, commit_message, commit_date
     except subprocess.CalledProcessError:
-        return "unknown", "unknown", "No commit message available", "Date unknown"
+        return "unknown", "No commit message available", "Date unknown"
