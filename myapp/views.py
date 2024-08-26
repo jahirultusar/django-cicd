e5de89919b8c714b065e9075dd17adc5bfc125c1
Jahirul Tusar <tusar86@gmail.com>
2024-08-26 19:22:22 +0100

    git tag test release

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 import subprocess
 
 
@@ -18,18 +18,18 @@ def index(request):
     return render(request, 'myapp/index.html', context)
 
 
-def get_git_info():
-    try:
-        commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()[:6]
-        log_output = subprocess.check_output(['git', 'log', '-1', '--pretty=%B%n%ad', '--date=local']).decode('ascii').strip()
-        commit_message, commit_date = log_output.split('\n', 1)  # Splits the output into message and date
-        latest_tag = subprocess.check_output(['git', 'describe', '--tags', '--abbrev=0']).decode('ascii').strip()
+# def get_git_info():
+#     try:
+#         commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()[:6]
+#         log_output = subprocess.check_output(['git', 'log', '-1', '--pretty=%B%n%ad', '--date=local']).decode('ascii').strip()
+#         commit_message, commit_date = log_output.split('\n', 1)  # Splits the output into message and date
+#         latest_tag = subprocess.check_output(['git', 'describe', '--tags', '--abbrev=0']).decode('ascii').strip()
 
-        return commit_hash, commit_message, commit_date, latest_tag
+#         return commit_hash, commit_message, commit_date, latest_tag
 
-    except subprocess.CalledProcessError:
-        # If there's an error, possibly because there are no tags yet, handle gracefully
-        return "unknown", "No commit message available", "Date unknown", "No tag available"
+#     except subprocess.CalledProcessError:
+#         # If there's an error, possibly because there are no tags yet, handle gracefully
+#         return "unknown", "No commit message available", "Date unknown", "No tag available"
 
 
 
@@ -44,3 +44,17 @@ def get_git_info():
 #         return "unknown", "No commit message available", "Date unknown"
 
 
+def get_git_info():
+    try:
+        commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()[:6]
+        log_output = subprocess.check_output(['git', 'log', '-1', '--pretty=%B%n%ad', '--date=local']).decode('ascii').strip()
+        commit_message, commit_date = log_output.split('\n', 1)
+        try:
+            # Attempt to fetch the latest tag
+            latest_tag = subprocess.check_output(['git', 'describe', '--tags', '--abbrev=0']).decode('ascii').strip()
+        except subprocess.CalledProcessError:
+            # If no tags could be described, handle gracefully
+            latest_tag = "No tag available"
+        return commit_hash, commit_message, commit_date, latest_tag
+    except subprocess.CalledProcessError:
+        return "unknown", "No commit message available", "Date unknown", "No tag available"
